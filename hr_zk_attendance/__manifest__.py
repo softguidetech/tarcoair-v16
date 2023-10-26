f35310e3ba209937319e89734f14f7e070da16d9
jamsheena kc <jamsheena.kc@ults.in>
2023-10-26 21:19:22 +0530

    cron hided

--- hr_zk_attendance/__manifest__.py
+++ hr_zk_attendance/__manifest__.py
@@ -33,7 +33,7 @@
         'security/ir.model.access.csv',
         'views/zk_machine_view.xml',
         'views/zk_machine_attendance_view.xml',
-        'data/download_data.xml'
+        # 'data/download_data.xml'
     ],
     'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
