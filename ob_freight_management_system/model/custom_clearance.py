dd8ba0c0fb62df6e559660618cad8041419e2ef0
softguidetech <96677990+softguidetech@users.noreply.github.com>
2025-03-14 23:31:26 +0400

    Update custom_clearance.py

--- ob_freight_management_system/model/custom_clearance.py
+++ ob_freight_management_system/model/custom_clearance.py
@@ -105,54 +105,55 @@ class CustomClearance(models.Model):
             rec.agent_id = rec.freight_id.agent_id
 
     def action_confirm(self):
-        """Send mail to inform agents to custom clearance is confirmed"""
-        for rec in self:
-            rec.name = 'CC' \
-                       ' - ' + rec.freight_id.name
-            rec.state = 'confirm'
-            base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-            Urls = urls.url_join(base_url, 'web#id=%(id)s&model=custom.clearance&view_type=form' % {'id': self.id})
-            Urls_ = urls.url_join(base_url, 'web#id=%(id)s&model=freight.order&view_type=form' % {'id': self.freight_id.id})
+        self.state = 'confirm'
+        # """Send mail to inform agents to custom clearance is confirmed"""
+        # for rec in self:
+        #     rec.name = 'CC' \
+        #               ' - ' + rec.freight_id.name
+        #     rec.state = 'confirm'
+        #     base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        #     Urls = urls.url_join(base_url, 'web#id=%(id)s&model=custom.clearance&view_type=form' % {'id': self.id})
+        #     Urls_ = urls.url_join(base_url, 'web#id=%(id)s&model=freight.order&view_type=form' % {'id': self.freight_id.id})
 
-            mail_content = _('Hi %s,<br>'
-                             'The Custom Clearance %s is confirmed'
-                             '<div style = "text-align: center; '
-                             'margin-top: 16px;"><a href = "%s"'
-                             'style = "padding: 5px 10px; font-size: 12px; '
-                             'line-height: 18px; color: #FFFFFF; '
-                             'border-color:#875A7B;text-decoration: none; '
-                             'display: inline-block; '
-                             'margin-bottom: 0px; font-weight: 400;'
-                             'text-align: center; vertical-align: middle; '
-                             'cursor: pointer; white-space: nowrap; '
-                             'background-image: none; '
-                             'background-color: #875A7B; '
-                             'border: 1px solid #875A7B; border-radius:3px;">'
-                             'View %s</a></div>'
-                             '<div style = "text-align: center; '
-                             'margin-top: 16px;"><a href = "%s"'
-                             'style = "padding: 5px 10px; font-size: 12px; '
-                             'line-height: 18px; color: #FFFFFF; '
-                             'border-color:#875A7B;text-decoration: none; '
-                             'display: inline-block; '
-                             'margin-bottom: 0px; font-weight: 400;'
-                             'text-align: center; vertical-align: middle; '
-                             'cursor: pointer; white-space: nowrap; '
-                             'background-image: none; '
-                             'background-color: #875A7B; '
-                             'border: 1px solid #875A7B; border-radius:3px;">'
-                             'View %s</a></div>'
-                             ) % (rec.agent_id.name, rec.name, Urls,
-                                  rec.name, Urls_, self.freight_id.name)
-            main_content = {
-                'subject': _('Custom Clerance For %s') % self.freight_id.name,
-                'author_id': self.env.user.partner_id.id,
-                'body_html': mail_content,
-                'email_to': rec.agent_id.email,
-            }
-            mail_id = self.env['mail.mail'].create(main_content)
-            mail_id.mail_message_id.body = mail_content
-            mail_id.send()
+        #     mail_content = _('Hi %s,<br>'
+        #                      'The Custom Clearance %s is confirmed'
+        #                      '<div style = "text-align: center; '
+        #                      'margin-top: 16px;"><a href = "%s"'
+        #                      'style = "padding: 5px 10px; font-size: 12px; '
+        #                      'line-height: 18px; color: #FFFFFF; '
+        #                      'border-color:#875A7B;text-decoration: none; '
+        #                      'display: inline-block; '
+        #                      'margin-bottom: 0px; font-weight: 400;'
+        #                      'text-align: center; vertical-align: middle; '
+        #                      'cursor: pointer; white-space: nowrap; '
+        #                      'background-image: none; '
+        #                      'background-color: #875A7B; '
+        #                      'border: 1px solid #875A7B; border-radius:3px;">'
+        #                      'View %s</a></div>'
+        #                      '<div style = "text-align: center; '
+        #                      'margin-top: 16px;"><a href = "%s"'
+        #                      'style = "padding: 5px 10px; font-size: 12px; '
+        #                      'line-height: 18px; color: #FFFFFF; '
+        #                      'border-color:#875A7B;text-decoration: none; '
+        #                      'display: inline-block; '
+        #                      'margin-bottom: 0px; font-weight: 400;'
+        #                      'text-align: center; vertical-align: middle; '
+        #                      'cursor: pointer; white-space: nowrap; '
+        #                      'background-image: none; '
+        #                      'background-color: #875A7B; '
+        #                      'border: 1px solid #875A7B; border-radius:3px;">'
+        #                      'View %s</a></div>'
+        #                      ) % (rec.agent_id.name, rec.name, Urls,
+        #                           rec.name, Urls_, self.freight_id.name)
+        #     main_content = {
+        #         'subject': _('Custom Clerance For %s') % self.freight_id.name,
+        #         'author_id': self.env.user.partner_id.id,
+        #         'body_html': mail_content,
+        #         'email_to': rec.agent_id.email,
+        #     }
+        #     mail_id = self.env['mail.mail'].create(main_content)
+        #     mail_id.mail_message_id.body = mail_content
+        #     mail_id.send()
 
     def action_revision(self):
         """Creating custom revision"""
